bf910e4f736e8a02939a5c7eab0d5fa2059c7f40
aaron <aaron.berton2004@gmail.com>
2026-04-27 10:50:13 +0200

    Implement action sampling and log probability calculation in Actor-Critic model

--- models/policy_network.py
+++ models/policy_network.py
@@ -12,19 +12,34 @@ class ActorCritic(tf.keras.Model):
         self.actor_mean = tf.keras.layers.Dense(act_dim)
         self.critic = tf.keras.layers.Dense(1)
 
+        self.log_std = tf.Variable(tf.zeros(act_dim), trainable=True)
+
     def call(self, obs):
         x = self.backbone(obs)
         mean = self.actor_mean(x)
         value = self.critic(x)
         return mean, value
 
+    def get_action(self, obs):
+        mean, value = self.call(obs)
+
+        std = tf.exp(self.log_std)
+        dist = tfp.distributions.Normal(mean, std)
+        action = dist.sample()
+        action = td.clip_by_value(action, -1.0, 1.0)
+        log_prob = tf.reduce_sum(dist.log_prob(action), axis=-1)
+        value = tf.squeeze(value, axis=-1)
+
+        return action, log_prob, value
+
 if __name__ == "__main__":
     import numpy as np
 
     model = ActorCritic(obs_dim=14, act_dim=4)
+    batch = np.random.randn(4, 14).astype(np.float32) #valeur random à remplacer par les vrais observations du walker
 
-    batch = np.random.randn(4, 14).astype(np.float32)
-    mean, value = model(batch)
+    action, log_prob, value = model.get_action(batch)
 
-    print("mean: ", mean.shape)
+    print("action: ", aciton.shape)
+    print("log_prob: ", log_prob.shape)
     print("value: ", value.shape)
