bbd9ac4e525d07b1628cad5904b9977220c62903
aaron <aaron.berton2004@gmail.com>
2026-04-27 10:55:10 +0200

    Fix TensorFlow Probability import in policy_network.py

--- models/policy_network.py
+++ models/policy_network.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 class ActorCritic(tf.keras.Model):
     def __init__(self, obs_dim, act_dim):
@@ -12,19 +13,34 @@ class ActorCritic(tf.keras.Model):
         self.actor_mean = tf.keras.layers.Dense(act_dim)
         self.critic = tf.keras.layers.Dense(1)
 
+        self.log_std = tf.Variable(tf.zeros(act_dim), trainable=True)
+
     def call(self, obs):
         x = self.backbone(obs)
         mean = self.actor_mean(x)
         value = self.critic(x)
         return mean, value
 
+    def get_action(self, obs):
+        mean, value = self.call(obs)
+
+        std = tf.exp(self.log_std)
+        dist = tfp.distributions.Normal(mean, std)
+        action = dist.sample()
+        action = tf.clip_by_value(action, -1.0, 1.0)
+        log_prob = tf.reduce_sum(dist.log_prob(action), axis=-1)
+        value = tf.squeeze(value, axis=-1)
+
+        return action, log_prob, value
+
 if __name__ == "__main__":
     import numpy as np
 
     model = ActorCritic(obs_dim=14, act_dim=4)
+    batch = np.random.randn(4, 14).astype(np.float32) #valeur random à remplacer par les vrais observations du walker
 
-    batch = np.random.randn(4, 14).astype(np.float32)
-    mean, value = model(batch)
+    action, log_prob, value = model.get_action(batch)
 
-    print("mean: ", mean.shape)
+    print("action: ", aciton.shape)
+    print("log_prob: ", log_prob.shape)
     print("value: ", value.shape)
